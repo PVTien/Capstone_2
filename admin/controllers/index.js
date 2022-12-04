49984a4a2b06698f14f429f0492cd1dd539f98d8
Tien Pham <tien.000.pro@gmail.com>
2022-12-05 04:56:55 +0700

    add admin feature

--- admin/controllers/index.js
+++ admin/controllers/index.js
@@ -1,3 +1,6 @@
+import { Product } from "../models/product.js";
+import { ProductService } from "../services/product.js";
+
 const productService = new ProductService();
 
 const domId = (id) => document.getElementById(id);
@@ -25,10 +28,10 @@ const renderProductList = (data) => {
               <button
                 data-toggle="modal"
                 data-target="#myModal"
+                onclick="openUpdateModal(${element.id})"
                 class="btn btn-info mr-3">Sửa</button>
               <button
-                data-toggle="modal"
-                data-target="#myModal"
+                onclick="deleteProduct(${element.id})"
                 class="btn btn-danger">Xóa</button>
             </td>
         </tr>
@@ -38,6 +41,98 @@ const renderProductList = (data) => {
   domId("tbodySanPham").innerHTML = html;
 };
 
+domId("btnThemSP").onclick = () => {
+  domId("exampleModalLabel").innerHTML = "Thêm sản phẩm";
+  domId("myForm").reset();
+  document.querySelector(".modal-footer").innerHTML =
+    "<button onclick='addProduct()' class='btn btn-primary'>Thêm</button>";
+};
+
+const addProduct = () => {
+  const name = domId("TenSP").value;
+  const price = domId("GiaSP").value;
+  const screen = domId("ManHinhSP").value;
+  const backCamera = domId("CameraSau").value;
+  const frontCamera = domId("CameraTruoc").value;
+  const img = domId("HinhSP").value;
+  const desc = domId("MoTaSP").value;
+  const type = domId("LoaiSP").value;
+
+  const product = new Product(
+    name,
+    price,
+    screen,
+    backCamera,
+    frontCamera,
+    img,
+    desc,
+    type
+  );
+
+  productService.addProd(product).then(() => {
+    document.querySelector(".close").click();
+    alert("Thêm sản phẩm thành công");
+    getProductList();
+  });
+};
+
+const deleteProduct = (id) => {
+  productService.deleteProd(id).then(() => {
+    alert("Xóa sản phẩm thành công");
+    getProductList();
+  });
+};
+
+const openUpdateModal = (id) => {
+  domId("exampleModalLabel").innerHTML = "Sửa sản phẩm";
+  document.querySelector(
+    ".modal-footer"
+  ).innerHTML = `<button onclick='updateProduct(${id})' class='btn btn-primary'>Sửa</button>`;
+
+  productService.getById(id).then((response) => {
+    domId("TenSP").value = response.data.name;
+    domId("GiaSP").value = response.data.price;
+    domId("ManHinhSP").value = response.data.screen;
+    domId("CameraSau").value = response.data.backCamera;
+    domId("CameraTruoc").value = response.data.frontCamera;
+    domId("HinhSP").value = response.data.img;
+    domId("MoTaSP").value = response.data.desc;
+    domId("LoaiSP").value = response.data.type;
+  });
+};
+
+const updateProduct = (id) => {
+  const name = domId("TenSP").value;
+  const price = domId("GiaSP").value;
+  const screen = domId("ManHinhSP").value;
+  const backCamera = domId("CameraSau").value;
+  const frontCamera = domId("CameraTruoc").value;
+  const img = domId("HinhSP").value;
+  const desc = domId("MoTaSP").value;
+  const type = domId("LoaiSP").value;
+
+  const product = new Product(
+    name,
+    price,
+    screen,
+    backCamera,
+    frontCamera,
+    img,
+    desc,
+    type
+  );
+
+  productService.updateProd(id, product).then(() => {
+    document.querySelector(".close").click();
+    alert("Sửa sản phẩm thành công");
+    getProductList();
+  });
+};
+
 window.onload = () => {
   getProductList();
 };
+window.addProduct = addProduct;
+window.deleteProduct = deleteProduct;
+window.openUpdateModal = openUpdateModal;
+window.updateProduct = updateProduct;
--- admin/services/product.js
+++ admin/services/product.js
@@ -1,8 +1,38 @@
-function ProductService() {
-  this.getList = () => {
+export class ProductService {
+  getList = () => {
     return axios({
       url: "https://6384edd93fa7acb14f04fa6f.mockapi.io/Products",
       method: "GET",
+    });
+  };
+
+  addProd = (data) => {
+    return axios({
+      url: "https://6384edd93fa7acb14f04fa6f.mockapi.io/Products",
+      method: "POST",
+      data: data,
+    });
+  };
+
+  deleteProd = (id) => {
+    return axios({
+      url: `https://6384edd93fa7acb14f04fa6f.mockapi.io/Products/${id}`,
+      method: "DELETE",
+    });
+  };
+
+  getById = (id) => {
+    return axios({
+      url: `https://6384edd93fa7acb14f04fa6f.mockapi.io/Products/${id}`,
+      method: "GET",
+    });
+  };
+
+  updateProd = (id, data) => {
+    return axios({
+      url: `https://6384edd93fa7acb14f04fa6f.mockapi.io/Products/${id}`,
+      method: "PUT",
+      data: data,
     });
   };
 }
